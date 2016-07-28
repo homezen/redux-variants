32d9ec3a303dcf7814e09dbf222ce34703404733
Mike S <SpainTrain@users.noreply.github.com>
2016-07-28 17:23:32 -0400

    :gift_heart: Extract variants tools into lib (fixes noref) (#1)
    - reducer for Redux
    - HoC for React
    - variant extractor for web servers
    - middleware for express
    
    connect https://github.com/home-buddy/homebuddy-webapp/issues/1589
    
    release v+major

---
+++ require-deps-whitelist.js
@@ -0,0 +1,12 @@
+/* eslint no-unreachable: 0 */
+
+throw new Error('This code should never be run.  Its purpose is to whitelist deps for `npm-check`')
+
+require('eslint')
+require('babel-loader')
+require('source-map-support')
+require('debug')
+require('es6-error')
+require('mocha-junit-reporter')
+require('nodemon')
+require('remark-lint')
